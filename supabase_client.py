e2dceea08dbb81397b095fb0336ecf9fc9345da2
JullyWang <hillhilkgreen42596@gmail.com>
2025-07-28 00:56:34 -0400

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -3,7 +3,7 @@ from supabase import create_client
 
 # Load secrets from .streamlit/secrets.toml
 url = st.secrets["SUPABASE_URL"]
-key = st.secrets["SUPABASE_KEY"]
+key = st.secrets["SUPABASE_PUBLIC_KEY"]
 
 supabase = create_client(url, key)
 
